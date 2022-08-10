67581b8d7701d11cccea5d2a874bc427999ae93f
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-10 19:29:57 -0300

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function form() {
+    let nombre_form = document.getElementById("nombre");
+    let apellido_form = document.getElementById("apellido");
+    let email_form = document.getElementById("email");
+    let password1_form = document.getElementById("password1");
+    let password2_form = document.getElementById("password2");
+    let terminos_form = document.getElementById("terminos");
+   
+}
