02061052e099be764b5b759635a513ed0c2be1db
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-17 11:41:09 -0300

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -6,12 +6,32 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-function form() {
-    let nombre_form = document.getElementById("nombre");
-    let apellido_form = document.getElementById("apellido");
-    let email_form = document.getElementById("email");
-    let password1_form = document.getElementById("password1");
-    let password2_form = document.getElementById("password2");
-    let terminos_form = document.getElementById("terminos");
-   
-}
+//function form() {
+  //  let nombre_form = document.getElementById("nombre");
+  //  let apellido_form = document.getElementById("apellido");
+ //   let email_form = document.getElementById("email");
+  //  let password1_form = document.getElementById("password1");
+//let password2_form = document.getElementById("password2");
+  //  let terminos_form = document.getElementById("terminos");
+   //
+//}
+
+const todo = document.getElementsByClassName("form-control")
+
+let boton = document.getElementById("regBtn");
+boton.addEventListener("click", () => {
+    let cantCompletas = 0;
+    for (por_cada of todo) {
+        
+        if (por_cada.value.length != 0) {
+            cantCompletas += 1;
+        }
+    }
+    if (cantCompletas == 5) {
+        alertSuccess();
+
+    } else {
+        alertError();
+
+    }
+});
